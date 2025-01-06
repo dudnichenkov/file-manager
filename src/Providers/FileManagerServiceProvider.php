d1792df833aee52c74f759eb5c0977497bea0e7b
dima <dmitriy.pidchenko@itgroup.kz>
2025-01-06 11:31:27 +0200

    Добавил и зарегистрировал FileManagerServiceProvider

---
+++ src/Providers/FileManagerServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dietrichxx\FileManager\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FileManagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
